831714e014532064ec64fd05eb66fae8755a5e70
Francisco Walter <franciscowalterd@gmail.com>
2023-10-29 23:53:44 -0300

    testando rotas para o github

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ import { Routes } from "./routes.js";
 
 const routes = new Routes();
 
-routes.add("/", "./pages/home/home.html");
-routes.add("/oUniverso", "./pages/oUniverso/oUniverso.html");
-routes.add("/exploracao", "./pages/exploracao/exploracao.html");
-routes.add(404, "./pages/notFound/notFound.html");
+routes.add("/", "/SPA-Universo/pages/home/home.html");
+routes.add("/oUniverso", "/SPA-Universo/pages/oUniverso/oUniverso.html");
+routes.add("/exploracao", "/SPA-Universo/pages/exploracao/exploracao.html");
+routes.add(404, "/SPA-Universo/pages/notFound/notFound.html");
 
 routes.handle();
 
--- js/routes.js
+++ js/routes.js
@@ -33,23 +33,23 @@ export class Routes {
         const header = document.querySelector("header");
         
         switch (route) {
-            case './pages/home/home.html':
+            case '/SPA-Universo/pages/home/home.html':
                 page.style.background = "url('./assets/home.png') no-repeat center /cover";
                 page.style.color = "#FFFFFF";
                 break;
 
-            case './pages/oUniverso/oUniverso.html':
+            case '/SPA-Universo/pages/oUniverso/oUniverso.html':
                 page.style.background = "url('./assets/universo.png') no-repeat center /cover";
                 page.style.color = "#FFFFFF";
                 break;
 
-            case './pages/exploracao/exploracao.html':
+            case '/SPA-Universo/pages/exploracao/exploracao.html':
                 page.style.background = "url('./assets/exploracao.png') no-repeat center /cover";
                 page.style.color = "#FFFFFF";
 
                 break;
             
-            case './pages/notFound/notFound.html':
+            case '/SPA-Universo/pages/notFound/notFound.html':
                 header.style.display = "none";
                 page.style.background = "#FFFFFF";
                 page.style.color = "#000000";
